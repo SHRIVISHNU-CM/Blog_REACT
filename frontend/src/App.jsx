699cc2ea6b67002b0093877b86a0d71f9ce9ba0b
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-13 23:00:14 +0530

    app file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+
+  return (
+    <>
+      <span className="loading loading-spinner"></span>
+      <button className="btn btn-success w-[50px]">Click</button>
+    </>
+  )
+}
+
+export default App
