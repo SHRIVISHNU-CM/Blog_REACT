1a5dcd45e7e18c7007836e3163f609a477238046
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-16 23:16:03 +0530

    Signin controller

---
+++ backend/Controllers/signin.js
@@ -0,0 +1,33 @@
+const dataUser = require('../Models/dataSchema')
+
+const signin = async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+        if (!username || !password || !email) {
+            return res.status(400).json({
+                message: "Enter all"
+            })
+        }
+        const user = await dataUser.findOne({ email: email })
+        if(user){
+            if(user.password != password){
+                return res.status(400).json({
+                    message:"Check password"
+                })
+            }
+            return res.status(200).json({
+                message:"signin"
+            })
+        }else{
+            return res.status(400).json({
+                message:"check email"
+            })
+        }
+
+
+    } catch (e) {
+        console.log(e.message)
+        return res.status(400).json(e.message)
+    }
+}
+module.exports = signin
