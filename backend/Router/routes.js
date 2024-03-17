0ba2b19ed54c9f1a0c0566acf63e720dad553b60
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-17 17:02:10 +0530

    jwt auth

--- backend/Router/routes.js
+++ backend/Router/routes.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const Router  = express.Router()
+const jwtauth = require('../Middleware/auth')
 const signup = require('../Controllers/signup')
 const signin = require('../Controllers/signin')
+const getAllData = require('../Controllers/GetAlldata')
+const logout = require('../Controllers/Logout')
 
 Router.post('/signup',signup)
 Router.post('/signin',signin)
-
+Router.get('/data',jwtauth,getAllData)
+Router.get('/logout',logout)
+// Router.get('/get',[jwtauth],getAllData)
 
 
 
