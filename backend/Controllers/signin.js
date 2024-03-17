e5cdb6be977b9674365f9066a095f04d943b2306
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-17 16:59:45 +0530

    generate token

--- backend/Controllers/signin.js
+++ backend/Controllers/signin.js
@@ -8,19 +8,29 @@ const signin = async (req, res) => {
                 message: "Enter all"
             })
         }
-        const user = await dataUser.findOne({ email: email })
-        if(user){
-            if(user.password != password){
+        
+        const user = await dataUser.findOne({ email: email }).select('+password')
+        if (user) {
+            if (user.password != password) {
                 return res.status(400).json({
-                    message:"Check password"
+                    message: "Check password"
                 })
             }
-            return res.status(200).json({
-                message:"signin"
+            const token = user.jwtToken();
+            user.password = undefined
+            const cookiesOptions = {
+                maxAge: 1*60*1000,
+                httpOnly:true
+            }
+            return res.cookie("token" , token,cookiesOptions).status(200).json({
+                message: "signin",
+                token:token
             })
-        }else{
+            
+
+        } else {
             return res.status(400).json({
-                message:"check email"
+                message: "check email"
             })
         }
 
