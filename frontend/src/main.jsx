651e33a730b16f6bc9f019ddb69882e9fd05aa46
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-18 21:57:45 +0530

    suspense

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 import App from './App.jsx'
 import './index.css'
 import Home from './Components/Home/MainPage/Home.jsx'
-import Login from './Components/Login/Login.jsx'
+const Login = lazy(() => import('./Components/Login/Login.jsx'))
+// import Login from './Components/Login/Login.jsx'
 import Signup from './Components/Signup/Signup.jsx'
+import Logout from './Components/Logout/Logout.jsx'
+import Loading from './Components/Loading/Loading.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route path='home' element={<Home />} />
-      <Route path='signin' element={<Login />} />
-      <Route path='signup' element={<Signup />} />
+      <Route path='home' element={
+        <Suspense fallback={<Loading />}>
+          <Home />
+        </Suspense>
+      } />
+      <Route path='signin' element={
+        <Suspense fallback={<Loading />}>
+          <Login />
+        </Suspense>
+      }
+
+      />
+      <Route path='signup' element={
+        <Suspense fallback={<Loading />}>
+          <Signup />
+        </Suspense>
+      } />
+      <Route path='logout' element={<Logout />} />
 
     </Route>
 
