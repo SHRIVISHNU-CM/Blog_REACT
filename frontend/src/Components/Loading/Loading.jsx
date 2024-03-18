c8a407db7ca4dd21f0bde07c32467c8bc21d364c
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-18 21:56:59 +0530

    Loading

---
+++ frontend/src/Components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <>
+        <h1>Hi vishnu CM</h1>
+        <span className='loading loading-spinner text-secondary loading-lg'></span>
+    </>
+  )
+}
+
+export default Loading
