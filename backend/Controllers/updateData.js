32ab92c2bdc9cb0c1d6f6f0bbb146d7af5a60c93
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-19 13:33:01 +0530

    Updat

---
+++ backend/Controllers/updateData.js
@@ -0,0 +1,23 @@
+const dataUser = require('../Models/dataSchema')
+
+
+const updataData =async (req,res)=>{
+    const {id} = req.params
+    try{
+        const users = await dataUser.findOneAndUpdate({_id:id},req.body)
+        const updated = await dataUser.findOne({_id:id})
+        console.log(updated)
+        console.log(users)
+        res.status(200).json({
+            message:users
+        })
+    }catch(e){
+        console.log(e.message)
+        res.send(400).json({
+            message:"invalid Id"
+        })
+    }
+
+}
+
+module.exports = updataData
