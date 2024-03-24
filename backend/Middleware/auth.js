ef783521e9eea4d7f32a080a5f8bb0a07eae6317
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-25 00:09:49 +0530

    auth

--- backend/Middleware/auth.js
+++ backend/Middleware/auth.js
@@ -1,26 +1,20 @@
 const jwt = require('jsonwebtoken')
 
 const jwtauth = (req,res,next) =>{
-    const token = (req.cookies && req.cookies.token) ||null;
+    let token = req.cookies.access_token;
+    if(!token) return res.status(400).json({
+            message:"Token Unavailable"})
 
-    if(!token){
-        return res.status(400).json({
-            message:"Invalid token"
+        jwt.verify(token,process.env.SECRET,(err,decoded)=>{
+            if(err){
+                return res.status(400).json({
+                    message:"Error in verification"
+                })
+            }else{
+                req.username = decoded.username
+                next()
+            }
         })
-    }
-    try{
-        const  payload = jwt.verify(token,process.env.SECRET);
-        req.user = {
-            email:payload.email,
-            id:payload.id
-        }
-
-    }catch(e){
-        console.log(e.message)
-        res.status(400).json({
-            message:"Error in midddleware"
-        })
-    }
-    next();
+      
 }
 module.exports = jwtauth
