88e5d1a89158786eec0712f3f47f3365a66c60bd
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-25 00:07:38 +0530

    routes

--- backend/Router/routes.js
+++ backend/Router/routes.js
@@ -4,13 +4,21 @@ const jwtauth = require('../Middleware/auth')
 const signup = require('../Controllers/signup')
 const signin = require('../Controllers/signin')
 const getAllData = require('../Controllers/GetAlldata')
+const userdata = require('../Controllers/userData')
 const logout = require('../Controllers/Logout')
+const updataData = require('../Controllers/updateData')
+const deleteData = require('../Controllers/deleteData')
+
+
 
 Router.post('/signup',signup)
 Router.post('/signin',signin)
-Router.get('/data',jwtauth,getAllData)
+Router.get('/data',getAllData)
+Router.get('/userdata/:id',userdata)
+Router.put('/update/:id',updataData)
+Router.delete('/delete/:id',deleteData)
 Router.get('/logout',logout)
-// Router.get('/get',[jwtauth],getAllData)
+
 
 
 
